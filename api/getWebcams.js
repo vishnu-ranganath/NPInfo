cb136665d7a84e1c9c11d24617e43e96340cf772
Vishnu Ranganath <vishnu.ranganath@utdallas.edu>
2021-10-31 18:48:59 -0500

    try env vars in js

--- api/getWebcams.js
+++ api/getWebcams.js
@@ -1,4 +1,4 @@
 module.exports = (req, res) => {
   const { name = 'World' } = req.query;
-  res.status(200).send(`Hello ${name}!`);
+  res.status(200).send(`Hello ${name}! \'` + process.env.NPS_API_TOKEN[0] + \');
 };
