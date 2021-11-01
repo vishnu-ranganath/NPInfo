8480716fbf01cb2a4aa532de3e86a77a7613980a
Vishnu Ranganath <vishnu.ranganath@utdallas.edu>
2021-10-31 20:00:25 -0500

    try using query string instead of writing

--- api/getWebcams.js
+++ api/getWebcams.js
@@ -1,20 +1,21 @@
 module.exports = (req, res) => {
 	
 	const https = require('https');
+	const querystring = require('querystring');
 	
 	//const { name = 'World' } = req.query;
-	
-	const options = {
-		hostname: 'developer.nps.gov',
-		path: '/api/v1/parks',
-		method: 'get'
-	};
 	
 	let requestData = "";
 	
 	const npsReqParams = {
 		parkCode: "acad",
 		api_key: process.env.NPS_API_TOKEN
+	};
+	
+	const options = {
+		hostname: 'developer.nps.gov',
+		path: '/api/v1/parks?' + querystring.stringify(npsReqParams),
+		method: 'get'
 	};
 	
 	const npsReq = https.request(options, npsRes => {
@@ -27,6 +28,5 @@ module.exports = (req, res) => {
 		res.status(200).send("API Call failed!");
 	});
 	
-	npsReq.write(new TextEncoder().encode(JSON.stringify(npsReqParams)));
 	npsReq.end();
 };
