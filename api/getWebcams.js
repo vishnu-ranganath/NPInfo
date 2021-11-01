b1a04b9ae618606e381aeccfb91a671017f51d28
Vishnu Ranganath <vishnu.ranganath@utdallas.edu>
2021-10-31 19:32:43 -0500

    attempt to call API

--- api/getWebcams.js
+++ api/getWebcams.js
@@ -1,4 +1,32 @@
 module.exports = (req, res) => {
-  const { name = 'World' } = req.query;
-  res.status(200).send(`Hello ${name}! \'` + process.env.NPS_API_TOKEN[0] + "\'");
+	
+	const https = require('https');
+	
+	//const { name = 'World' } = req.query;
+	
+	const options = {
+		hostname: 'developer.nps.gov',
+		path: '/api/v1/parks',
+		method: 'get'
+	};
+	
+	let requestData = "";
+	
+	const npsReqParams = {
+		parkCode: "acad",
+		api_key: process.env.API_URL
+	};
+	
+	const npsReq https.request(options, npsRes => {
+		npsRes.on("data", d => {
+			res.status(200).send(d);
+		});
+	});
+	
+	npsReq.on('error', error => {
+		res.status(200).send("API Call failed!");
+	});
+	
+	npsReq.write(new TextEncoder().encode(JSON.stringify(npsReqParams)));
+	npsReq.end();
 };
