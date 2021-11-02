b0c2c23b987d1dcaa53762e9d95d9605796a35c9
Vishnu Ranganath <vishnu.ranganath@utdallas.edu>
2021-11-02 13:33:44 -0500

    prepare to call APIs

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
-import './api';
+import {apiKey} from './api';
 import './App.css';
 
 function App() {
+	if(apiKey === undefined) {
+		return (
+			<div className = "App">
+				API Token Undefined!
+			</div>
+		);
+	}
 	return (
 		<div className="App">
 			Hello, world! -{apiKey[0] + apiKey[2]}
--- src/api.js
+++ src/api.js
@@ -1,1 +1,4 @@
+//The API token is to be stored as an environment variable on the server
 const apiKey = process.env.NPS_API_TOKEN;
+
+export {apiKey};
