3279b54b824df87f7ae92aecac6bee37d2759815
Vishnu Ranganath <vishnu.ranganath@utdallas.edu>
2021-11-02 16:52:51 -0500

    call the activities api

--- src/AllActivities.js
+++ src/AllActivities.js
@@ -1,19 +1,54 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
 import {apiKey} from './api';
 import './App.css';
 
-function AllActivities() {
-	if(apiKey === undefined) {
+class AllActivities extends React.Component {
+	
+	constructor(props) {
+		super(props);
+		this.state = {activityList: []};
+	}
+	
+	componentDidMount() {
+		const allActReq = new XMLHttpRequest();
+		allActReq.onreadystatechange = () => {
+			if(allActReq.readyState != XMLHttpRequest.DONE) {
+				return;
+			} else if(allActReq.status == 200) {
+				let activityData = JSON.parse(allActReq.responseText);
+				this.setState({activityList: activityData.data});
+			}
+		}
+		allActReq.open("GET", "https://developer.nps.gov/api/v1/activities?api_key=" + apiKey, true);
+		allActReq.send();
+	}
+	
+	componentWillUnmount() {
+		
+	}
+
+	render() {
+		if(apiKey === undefined) {
+			return (
+				<div className="AllActivities">
+					No token!
+				</div>
+			);
+		}
+		
+		const activities = this.state.activityList.map((a) =>
+			<li>{a.name}</li>
+		);
+		
 		return (
-			<div className = "AllActivities">
-				API Token Undefined!
+			<div className="AllActivities">
+				<ul>{activities}</ul>
 			</div>
 		);
+		
 	}
-	return (
-		<div className="AllActivities">
-			Hello, world! -{apiKey[0] + apiKey[2]}
-		</div>
-	);
+
 }
 
 export default AllActivities;
