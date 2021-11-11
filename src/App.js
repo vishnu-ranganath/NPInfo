95e209f161fe794326148b96a546fa04beee1aa1
Vishnu Ranganath <vishnu.ranganath@utdallas.edu>
2021-11-11 13:42:34 -0600

    break App into components

--- src/App.js
+++ src/App.js
@@ -1,60 +1,20 @@
 import './App.css';
 import React from 'react';
-import {apiKey} from './api';
+import ParksByActivity from './ParksByActivity';
 
 class App extends React.Component {
 	
 	constructor(props) {
 		super(props);
-		this.state = {activityList: [], parks: []};
-	}
-	
-	componentDidMount() {
-		const allActReq = new XMLHttpRequest();
-		allActReq.onreadystatechange = () => {
-			if(allActReq.readyState !== XMLHttpRequest.DONE) {
-				return;
-			} else if(allActReq.status === 200) {
-				let activityData = JSON.parse(allActReq.responseText);
-				this.setState({activityList: activityData.data});
-			}
-		}
-		allActReq.open("GET", "https://developer.nps.gov/api/v1/activities?api_key=" + apiKey, true);
-		allActReq.send();
+		this.state = {parks: []};
+		this.setParks = this.setParks.bind(this);
 	}
 
-	handleChange = (event) => {
-		const activity = event.target.value;
-		const parksByActReq = new XMLHttpRequest();
-		parksByActReq.onreadystatechange = () => {
-			if(parksByActReq.readyState !== XMLHttpRequest.DONE) {
-				return;
-			} else if(parksByActReq.status === 200) {
-				let parkData = JSON.parse(parksByActReq.responseText);
-				this.setState({parks: parkData.data[0].parks});
-			}
-		}
-		console.log(activity);
-		parksByActReq.open("GET", "https://developer.nps.gov/api/v1/activities/parks?api_key=" + apiKey + "&id=" + activity, true);
-		parksByActReq.send();
+	setParks(parkList) {
+		this.setState({parks: parkList});
 	}
 	
 	render() {
-		if(apiKey === undefined) {
-			return (
-				<div className="AllActivities">
-					No token!
-				</div>
-			);
-		}
-		
-		const activities = this.state.activityList.map((a) =>
-			<option value={a.id}>{a.name}</option>
-		);
-		
-		activities.unshift(
-			<option value={"blank"} hidden={true}>Select an activity</option>
-		);
 
 		const parks = this.state.parks.map((a) =>
 			<option value={a.parkCode}>{a.name}</option>
@@ -66,9 +26,8 @@ class App extends React.Component {
 		
 		return (
 			<div className="App">
-				<select onChange={this.handleChange}>{activities}</select>
-				<br/>
-				<select>{parks}</select>
+				<ParksByActivity setParks={this.setParks}/>
+				<select id={"parkSelector"}>{parks}</select>
 			</div>
 		);
 		
---
+++ src/ParksByActivity.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import {apiKey} from './api';
+
+class ParksByActivity extends React.Component {
+	
+	constructor(props) {
+		super(props);
+		this.state = {activityList: [], parks: []};
+		this.handleChange = this.handleChange.bind(this);
+	}
+	
+	componentDidMount() {
+		const allActReq = new XMLHttpRequest();
+		allActReq.onreadystatechange = () => {
+			if(allActReq.readyState !== XMLHttpRequest.DONE) {
+				return;
+			} else if(allActReq.status === 200) {
+				let activityData = JSON.parse(allActReq.responseText);
+				this.setState({activityList: activityData.data});
+			}
+		}
+		allActReq.open("GET", "https://developer.nps.gov/api/v1/activities?api_key=" + apiKey, true);
+		allActReq.send();
+	}
+
+	handleChange(event) {
+		const activity = event.target.value;
+		const parksByActReq = new XMLHttpRequest();
+		parksByActReq.onreadystatechange = () => {
+			if(parksByActReq.readyState !== XMLHttpRequest.DONE) {
+				return;
+			} else if(parksByActReq.status === 200) {
+				let parkData = JSON.parse(parksByActReq.responseText);
+				this.props.setParks(parkData.data[0].parks);
+			}
+		}
+		parksByActReq.open("GET", "https://developer.nps.gov/api/v1/activities/parks?api_key=" + apiKey + "&id=" + activity, true);
+		parksByActReq.send();
+	}
+	
+	render() {
+		
+		const activities = this.state.activityList.map((a) =>
+			<option value={a.id}>{a.name}</option>
+		);
+		
+		activities.unshift(
+			<option value={"blank"} hidden={true}>Select an activity</option>
+		);
+		
+		return (
+			<div className="ParksByActivity">
+				<select onChange={this.handleChange} id={"activitySelector"}>{activities}</select>
+			</div>
+		);
+		
+	}
+
+}
+
+export default ParksByActivity;
